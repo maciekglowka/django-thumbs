59f8c7d6abeb2611fb23a720265a4352d5b99352
maciekglowka <maciek.glowka@gmail.com>
2021-09-16 10:55:31 +0200

    first deploy

--- img_thumbs/img_thumbs/settings.py
+++ img_thumbs/img_thumbs/settings.py
@@ -30,10 +30,6 @@ SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', False)
 
-if not DEBUG:
-    SESSION_COOKIE_SECURE = True
-    CSRF_COOKIE_SECURE = True
-
 ALLOWED_HOSTS = ['*']
 
 
